e70d93e927ecbdc57d9300de9f0e9ea227acee61
gookieAT <36005381+gookieAT@users.noreply.github.com>
2021-10-24 19:16:22 +0200

    remove debug stuff

--- web.py
+++ web.py
@@ -64,7 +64,6 @@ def profile():
             if p["name"] == g.name and p["tag"] == g.tag:
                 if i == 0:
                     g.playertitle = valorant.get_playertitle(p["player_title"])["data"]["titleText"]
-                    print(g.playertitle)
                 exec(f'g.match{i + 1}_agent_icon = p["assets"]["agent"]["killfeed"]')
                 exec(f'g.match{i + 1}_agent_name = p["character"]')
                 exec(f'g.match{i + 1}_score = p["stats"]["score"]')
